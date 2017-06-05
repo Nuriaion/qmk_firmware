92b7e735bd38dab2a684c76b75c07415d86b31ad
Fabian Page <faebu3007@gmail.com>
2017-06-05 19:07:42 +0200

    wip

---
+++ keyboards/atreus62/keymaps/fabian/config.h
@@ -0,0 +1,16 @@
+#define ONESHOT_TIMEOUT 3000
+#define TAPPING_TERM 200
+#define PREVENT_STUCK_MODIFIERS
+#define FORCE_NKRO
+#define LEADER_TIMEOUT 1000
+
+#include "../../config.h"
+
+#define LSPO_KEY KC_8
+#define RSPC_KEY KC_9
+
+#define MOUSEKEY_INTERVAL       20
+#define MOUSEKEY_DELAY          0
+#define MOUSEKEY_TIME_TO_MAX    60
+#define MOUSEKEY_MAX_SPEED      7
+#define MOUSEKEY_WHEEL_DELAY 0
--- keyboards/atreus62/keymaps/fabian/keymap.c
+++ keyboards/atreus62/keymaps/fabian/keymap.c
@@ -10,24 +10,48 @@
 // Layer names don't all need to be of the same length, obviously, and you can also skip them
 // entirely and just use numbers.
 #define _DEFAULT 0
-#define _NAV 1
-#define _RESET 2
+#define _LEFT 1
+#define _RIGHT 2
+#define _RESET 3
+
+#define LCTRLP MT(KC_LCTRL, CH_LPRN)
+
+//Tap Dance Declarations
+enum {
+  TD_CTRL_PO = 0
+};
+
+//Tap Dance Definitions
+qk_tap_dance_action_t tap_dance_actions[] = {
+  //Tap once for Esc, twice for Caps Lock
+  [TD_CTRL_PO]  = ACTION_TAP_DANCE_DOUBLE(KC_ESC, KC_CAPS)
+// Other declarations would go here, separated by commas, if you have them
+};
+
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_DEFAULT] = { /* qwerty */
-		{ KC_EQL,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_TRNS,         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS },
-		{ KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_TRNS,         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    CH_UE },
-		{ KC_LCTL,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_TRNS,         KC_H,    KC_J,    KC_K,    KC_L,    CH_OE,   CH_AE },
-		{ KC_LSFT,  CH_Y,    KC_X,    KC_C,    KC_V,    KC_B,    KC_BSPC,         KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSHIFT },
-		{ KC_LCTL,  KC_LGUI, KC_LALT, KC_GRV,  MO(_NAV),KC_ESC,          KC_ENT, KC_SPC,  KC_EQL,  KC_MINS, KC_QUOT, KC_ENT,  KC_RGUI }
+		{ CH_PARA,  KC_1,    KC_2,    KC_3,    KC_4,       KC_5,    KC_TRNS,         KC_6,    KC_7,        KC_8,    KC_9,    KC_0,    CH_QUOT },
+		{ KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,       KC_T,    KC_TRNS,         KC_Y,    KC_U,        KC_I,    KC_O,    KC_P,    CH_CARR },
+		{ KC_LCTRL, KC_A,    KC_S,    KC_D,    KC_F,       KC_G,    KC_TRNS,         KC_H,    KC_J,        KC_K,    KC_L,    CH_OE,   KC_RCTRL },
+		{ KC_LSPO,  CH_Y,    KC_X,    KC_C,    KC_V,       KC_B,    KC_BSPC,         KC_N,    KC_M,        KC_COMM, KC_DOT,  KC_SLSH, KC_RSPC },
+		{ KC_LALT,  CH_LESS, KC_LALT, KC_LGUI, OSL(_LEFT), KC_ESC,           KC_ENT, KC_SPC,  OSL(_RIGHT), KC_RGUI, KC_QUOT, KC_ENT,  CH_ALGR }
 },
 
-[_NAV] = { 
-		{ TO(_DEFAULT),  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_TRNS,           KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11  },
-		{ KC_TRNS,       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,           KC_F12,  KC_TRNS, KC_UP,   KC_TRNS, KC_TRNS, KC_TRNS },
-		{ KC_TRNS,       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,           KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_TRNS, KC_TRNS },
-		{ TO(_RESET),    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,           KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS },
-		{ KC_TRNS,       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS }
+[_LEFT] = { 
+		{ TO(_RESET), KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_TRNS,           KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11  },
+		{ KC_TRNS,    KC_TRNS, KC_7,    KC_8,    KC_9,    KC_TRNS, KC_TRNS,           KC_F12,  KC_TRNS, KC_UP,   KC_TRNS, KC_TRNS, CH_UE },
+		{ KC_TRNS,    KC_TRNS, KC_4,    KC_5,    KC_6,    KC_0,    KC_TRNS,           KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, CH_OE,   CH_AE },
+		{ KC_TRNS,    KC_TRNS, KC_1,    KC_2,    KC_3,    KC_TRNS, KC_TRNS,           KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS },
+		{ KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS }
+},
+
+[_RIGHT] = {
+		{ KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS },
+		{ KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,            KC_TRNS, CH_LPRN, CH_RPRN, KC_TRNS, KC_TRNS, KC_TRNS },
+		{ KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,            KC_TRNS, CH_LCBR, CH_RCBR, KC_TRNS, KC_TRNS, KC_TRNS },
+		{ KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,            KC_TRNS, CH_LBRC, CH_RBRC, KC_TRNS, KC_TRNS, KC_TRNS },
+		{ KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,           KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS }
 },
 
 [_RESET] = {
